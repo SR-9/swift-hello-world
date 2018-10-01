818344465beac16126038c95ed1685c1ff4db69b
Tran Van Hieu <hieutv@evolableasia.vn>
2018-10-01 11:20:51 +0700

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Van Hieu on 10/1/18.
+//  Copyright © 2018 Van Hieu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
